03ee7e988cd9924994e7261f25cd1f98f4ad7b06
vt4a2h <vt4a2h@gmail.com>
2014-01-22 19:03:27 +0700

    added 2d array

--- arrays.h
+++ arrays.h
@@ -3,7 +3,7 @@
 #include <algorithm>
 #include <stdio.h>
 
-#define CHECK_AND_RETURN if (index >= arrayLength) throw BadIndex(); return m_Array[index];
+#define CHECK_AND_RETURN(len, index) if (index >= len) throw BadIndex(); return m_Array[index];
 
 namespace fixed_arrays {
 
@@ -15,17 +15,15 @@ namespace fixed_arrays {
     {
     public:
         FixedArray() :  m_Array(new Type[arrayLength]), m_Size(arrayLength) {}
-        FixedArray(FixedArray &&src) { move(src, this); }
         FixedArray(std::initializer_list<Type> l);
-        FixedArray(const FixedArray &src) { copy(src, this); }
-        virtual ~FixedArray() { delete []m_Array; }
+        FixedArray(const FixedArray &src) { copy(src, *this); }
+        virtual ~FixedArray() {delete []m_Array; }
 
-        FixedArray& operator=(FixedArray &&rhs);
         FixedArray& operator=(const FixedArray &rhs);
-        Type& operator[](size_t index)             { CHECK_AND_RETURN }
-        const Type& operator[](size_t index) const { CHECK_AND_RETURN }
+        Type& operator[](size_t index)             { CHECK_AND_RETURN(arrayLength, index) }
+        const Type& operator[](size_t index) const { CHECK_AND_RETURN(arrayLength, index) }
 
-        const Type& at(size_t index) const { CHECK_AND_RETURN }
+        const Type& at(size_t index) const { CHECK_AND_RETURN(arrayLength, index) }
         size_t size() const { return m_Size; }
         bool empty()  const { return m_Size != 0 ? true : false; }
 
@@ -35,9 +33,10 @@ namespace fixed_arrays {
         Type* end() { return m_Array + m_Size;}
         const Type* end() const { return m_Array + m_Size;}
 
+        FixedArray(FixedArray &&src) = delete;
+        FixedArray& operator=(FixedArray &&rhs) = delete;
     private:
         void copy(const FixedArray &src, FixedArray &dst);
-        void move(FixedArray &&src, FixedArray &dst);
 
         Type *m_Array;
         size_t m_Size;
@@ -51,22 +50,15 @@ namespace fixed_arrays {
         std::copy(l.begin(), l.end(), begin());
     }
 
+
     template <class Type, size_t arrayLength>
     FixedArray<Type, arrayLength>& FixedArray<Type, arrayLength>::operator=(const FixedArray &rhs)
     {
         if (this != &rhs)
+            delete []m_Array;
             copy(rhs, *this);
 
         return *this;
-    }
-
-    template <class Type, size_t arrayLength>
-    FixedArray<Type, arrayLength>& FixedArray<Type, arrayLength>::operator=(FixedArray &&rhs)
-    {
-        if (this != &rhs)
-            move(rhs, *this);
-
-        return this;
     }
 
     template <class Type, size_t arrayLength>
@@ -74,20 +66,73 @@ namespace fixed_arrays {
     {
         dst.m_Size = src.m_Size;
 
-        delete []dst.m_Array;
         m_Array = new Type[m_Size];
         std::copy(src.begin(), src.end(), begin());
     }
 
-    template <class Type, size_t arrayLength>
-    void FixedArray<Type, arrayLength>::move(FixedArray &&src, FixedArray &dst)
+
+    /*! \brief Класс FixedArray2D представляет собой реализацию двумерного статического массива. */
+    template<class Type, size_t N, size_t M>
+    class FixedArray2D
     {
-        delete []dst.m_Array;
+    public:
+        FixedArray2D() : m_Array(new FixedArray<Type, N>[M]), m_Size(M) {}
+        FixedArray2D(std::initializer_list<FixedArray<Type, N>> l);
+        FixedArray2D(const FixedArray2D &src) { copy(src, *this); }
+        virtual ~FixedArray2D() { delete []m_Array;}
 
-        dst.m_Array = src.begin();
-        src.m_Array = nullptr;
+        FixedArray2D& operator=(const FixedArray2D &rhs);
+        FixedArray<Type, N>& operator[](size_t index)             { CHECK_AND_RETURN(M, index) }
+        const FixedArray<Type, N>& operator[](size_t index) const { CHECK_AND_RETURN(M, index) }
 
-        dst.m_Size = src.size();
-        src.m_Size = 0;
+        const FixedArray<Type, N>& at(size_t index) const { CHECK_AND_RETURN(M, index) }
+        size_t rowCount() const { return m_Size; }
+        size_t colCount() const { return N; }
+        bool empty()  const { return m_Size != 0 ? true : false; }
+
+        FixedArray<Type, N>* begin() { return m_Array;}
+        const FixedArray<Type, N>* begin() const { return m_Array;}
+
+        FixedArray<Type, N>* end() { return m_Array + m_Size;}
+        const FixedArray<Type, N>* end() const { return m_Array + m_Size;}
+
+        FixedArray2D(FixedArray2D &&src) = delete;
+        FixedArray2D& operator=(FixedArray2D &&rhs) = delete;
+
+    private:
+        void copy(const FixedArray2D &src, FixedArray2D &dst);
+
+        FixedArray<Type, N> *m_Array;
+        size_t m_Size;
+    };
+
+    template<class Type, size_t N, size_t M>
+    FixedArray2D<Type, N, M>::FixedArray2D(std::initializer_list<FixedArray<Type, N>> l)
+        : m_Array(new FixedArray<Type, N>[M])
+        , m_Size(M)
+    {
+        std::copy(l.begin(), l.end(), begin());
     }
+
+
+    template<class Type, size_t N, size_t M>
+    FixedArray2D<Type, N, M>& FixedArray2D<Type, N, M>::operator=(const FixedArray2D &rhs)
+    {
+        if (this != &rhs)
+            delete []m_Array;
+            copy(rhs, *this);
+
+        return *this;
+    }
+
+
+    template<class Type, size_t N, size_t M>
+    void FixedArray2D<Type, N, M>::copy(const FixedArray2D &src, FixedArray2D &dst)
+    {
+        dst.m_Size = src.m_Size;
+
+        m_Array = new FixedArray<Type, N>[m_Size];
+        std::copy(src.begin(), src.end(), begin());
+    }
+
 }
--- main.cpp
+++ main.cpp
@@ -1,5 +1,37 @@
+#include "arrays.h"
+#include <iomanip>
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <iterator>
+
 int main()
 {
+    // Примеры использования одномерного статического массива.
+    fixed_arrays::FixedArray<int, 5> arr1 {5, 2, 4, 1, 8};
+    std::copy(arr1.begin(), arr1.end(), std::ostream_iterator<int>(std::cout, " "));
+    std::cout << std::endl;
+
+    fixed_arrays::FixedArray<int, 5> arr2(arr1);
+    std::copy(arr2.begin(), arr2.end(), std::ostream_iterator<int>(std::cout, " "));
+    std::cout << std::endl;
+
+    std::transform(arr2.begin(), arr2.end(), arr1.begin(), [](int &i){ return i*2; });
+    std::copy(arr1.begin(), arr1.end(), std::ostream_iterator<int>(std::cout, " "));
+    std::cout << std::endl;
+
+    // Примеры использования двумероного статического массива.
+     fixed_arrays::FixedArray2D<int, 3, 2> arr2d{ {7, 5, 1}, {12, 30, 8} };
+
+     std::for_each(arr2d.begin(), arr2d.end(), [](decltype(arr2d[0]) a){std::sort(a.begin(), a.end());} );
+
+     std::for_each(arr2d.begin(), arr2d.end(),
+        [](decltype(arr2d[0]) a){
+            std::copy(a.begin(), a.end(), std::ostream_iterator<int>(std::cout, " "));
+            std::cout << std::endl;
+        }
+     );
+
     return 0;
 }
 
